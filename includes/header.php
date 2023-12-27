8307b0134599b95866f35ff5ae3585fedc06c764
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2023-12-27 22:39:13 +0700

    update header

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+    <!-- <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SB Admin</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="assets/css/styles.css" rel="stylesheet" />
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    </head> -->
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -18,8 +30,7 @@
 
         <div id="layoutSidenav">
 
-            <?php include('sidebar.php');?>
-
+            
                 <div id="layoutSidenav_content">
                     
                     <main>
