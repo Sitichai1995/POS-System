cc8768e8be2d54adfba984ab7c3b08ea9306edb3
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2023-12-28 22:25:37 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php include('includes/header.php')?>
+
   
 <div class="py-5 bg-light">
     <div class="container mt-5">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card shadow rounded-4">
+                    <?php alertMessage();?>
                     <div class="p-5">
                         <h4 class="text-dark mb-3">Sign into your POS system</h4>
                         <form action="login-code.php" method="POST">
