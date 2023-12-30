cb74cf772888e4b9ef49e6287f295c02165e5203
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2023-12-30 14:12:47 +0700

    Create category-create.php

---
+++ admin/category-create.php
@@ -0,0 +1,40 @@
+<?php 
+    include('includes/header.php'); 
+?>
+  
+    <div class="container-fluid px-4">
+        <div class="card mt-4 shadow-md">
+            <div class="card-header">
+                <h4 class="mb-0">Add Category
+                    <a href="admins.php" class="btn btn-primary float-end">Back</a>
+                </h4>
+
+            </div>
+        </div>
+        <div class="card-body">
+            <?php alertMessage();?>
+            <form action="code.php" method="POST">
+                <div class="row">
+                    <div class="col-md-12 mb-3">
+                        <label for="">Name *</label>
+                        <input type="text" name="name" required class="form-control" />
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Description</label>
+                        <textarea name="description" id=""rows="3" class="form-control"></textarea>
+                    </div>
+                    <div class="col-md-6">
+                        <label for="">Status (checked = Hidden, unchecked = visible)</label>
+                        <br/>
+                        <input type="checkbox" name="status" style="width: 30px; height: 30px;" class="">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <br/>
+                        <button type="submit" name="saveCategory" class="btn btn-success">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+<?php include('includes/footer.php')?>
