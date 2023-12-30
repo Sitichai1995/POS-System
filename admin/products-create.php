a74f67b789faa460e86c0a9c85a596ad4cf5d92e
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2023-12-30 22:38:47 +0700

    Update products-create.php

--- admin/products-create.php
+++ admin/products-create.php
@@ -13,16 +13,51 @@
         </div>
         <div class="card-body">
             <?php alertMessage();?>
-            <form action="code.php" method="POST">
+            <form action="code.php" method="POST" enctype="multipart/form-data">
                 <div class="row">
+                    <div class="col-md-12">
+                        <label > Select Category</label>
+                        <select name="category_id" class="form-select">
+                            <option value="">Select Category</option>
+                            <?php 
+                            $categories = getAll('categories');
+                            if ($categories){
+                                if (mysqli_num_rows($categories) > 0) {
+                                    foreach ($categories as $category) {
+                                        echo '<option value="'.$category['id'].'">'.$category['name'].'</option>';
+                                    }
+                                } else {
+                                    echo '<option value="">No Categories found.</option>';
+                                }
+                                
+
+                            }else{
+                                echo '<option value="">Something went wrong.</option>';
+                            }
+                            ?>
+                        </select>
+                    </div>
                     <div class="col-md-12 mb-3">
-                        <label for="">Name *</label>
+                        <label for="">Product Name *</label>
                         <input type="text" name="name" required class="form-control" />
                     </div>
                     <div class="col-md-12 mb-3">
                         <label for="">Description</label>
                         <textarea name="description" id=""rows="3" class="form-control"></textarea>
                     </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="">Price</label>
+                        <input type="text" name="price" required class="form-control" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="">Quantity</label>
+                        <input type="text" name="quatity" required class="form-control" />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label for="">Image</label>
+                        <input type="file" name="image" required class="form-control" />
+                    </div>
+                   
                     <div class="col-md-6">
                         <label for="">Status (checked = Hidden, unchecked = visible)</label>
                         <br/>
@@ -30,7 +65,7 @@
                     </div>
                     <div class="col-md-6 mb-3">
                         <br/>
-                        <button type="submit" name="saveCategory" class="btn btn-success">Save</button>
+                        <button type="submit" name="saveProduct" class="btn btn-success">Save</button>
                     </div>
                 </div>
             </form>
