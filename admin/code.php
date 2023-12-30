ea2bd3f1c116f788f8266259de2c6fe43d89a941
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2023-12-30 22:38:45 +0700

    Update code.php

--- admin/code.php
+++ admin/code.php
@@ -134,5 +134,51 @@ if (isset($_POST['UpdateCategory'])) {
 }
 
 
+//Products
+
+if (isset($_POST['saveProduct'])) {
+
+    $category_id = validate($_POST['category_id']);
+    $name = validate($_POST['name']);
+    $description = validate($_POST['description']);
+    $price = validate($_POST['price']);
+    $quantity = validate($_POST['quantity']);
+    $status = isset($_POST['status']) == true ? 1 : 0;
+
+    if ($_FILES['image']['size'] >0) {
+        $path = "../assets/uploads/products";
+        $image_ext = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+
+        $filename = time().'.'.$image_ext;
+
+        move_uploaded_file($_FILES['image']['tmp_name'], $path.'/'.$filename);
+
+        $finalImage = 'assets/uploads/products'.$filename;
+        // echo $finalImage;
+        // exit();
+    } else {
+        $finalImage = '';
+    }
+    
+
+    $data = [
+        'category_id' => $category_id,
+        'name' => $name,
+        'description' => $description,
+        'price' => $price,
+        'quantity' => $quantity,
+        'image' => $finalImage,
+        'status' => $status,
+    ];
+    // var_dump($data);
+    // exit();
+    $result = insert('products', $data);
+
+    if ($result) {
+        redirect('products.php', 'products Created Successfully');
+    } else {
+        redirect('products-create.php', 'Something Went Wrong!');
+    }
+}
 
 
