0f92fa3238f4716da3b2f46dfb0ec0949557652e
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2024-01-04 23:09:51 +0700

    Update order-code.php

--- admin/order-code.php
+++ admin/order-code.php
@@ -18,7 +18,7 @@ if (isset($_POST['addItem'])) {
     $productId = validate($_POST['productId']);
     $quantity = validate($_POST['quantity']);
 
-    
+
     $checkProduct = mysqli_query($conn, "SELECT * FROM products WHERE id = '$productId' limit 1 ");
 
     if ($checkProduct) {
@@ -26,7 +26,7 @@ if (isset($_POST['addItem'])) {
 
             $row = mysqli_fetch_assoc($checkProduct);
             if ($row['quantity'] < $quantity) {
-                redirect('order-create.php', 'Only' .$row['quantity']. 'quantity avaliable!');
+                redirect('order-create.php', 'Only' . $row['quantity'] . 'quantity avaliable!');
             }
 
             $productData = [
@@ -38,14 +38,14 @@ if (isset($_POST['addItem'])) {
             ];
 
             if (!in_array($row['id'], $_SESSION['productItemIds'])) {
-                
-                array_push($_SESSION['productItemIds'],$row['id']);
-                array_push($_SESSION['productItems'],$productData);
-            }else{
+
+                array_push($_SESSION['productItemIds'], $row['id']);
+                array_push($_SESSION['productItems'], $productData);
+            } else {
                 foreach ($_SESSION['productItems'] as $key => $productSessionItem) {
                     if ($productSessionItem['product_id'] == $row['id']) {
-                        
-                        $newQuantity = $productSessionItem['quantity']+ $quantity;
+
+                        $newQuantity = $productSessionItem['quantity'] + $quantity;
 
                         $productData = [
                             'production_id' => $row['id'],
@@ -59,10 +59,8 @@ if (isset($_POST['addItem'])) {
                     }
                 }
             }
-            redirect('order-create.php', 'Item Added ' .$row['name']);
-
-            
-        }else{
+            redirect('order-create.php', 'Item Added ' . $row['name']);
+        } else {
             redirect('order-create.php', 'No such product found.');
         }
     } else {
@@ -84,15 +82,41 @@ if (isset($_POST['productIncDec'])) {
 
     if ($flag) {
 
-        jsonResponse (200, 'success', 'Quantity Updated');
-    }else{
+        jsonResponse(200, 'success', 'Quantity Updated');
+    } else {
 
-        jsonResponse (500, 'error', 'something went wrong.');
+        jsonResponse(500, 'error', 'something went wrong.');
     }
 }
 
 
 if (isset($_POST['proceedToPlaceBtn'])) {
     $cPhone = validate($_POST['cPhone']);
-    //next here
+    $paymentMode = validate($_POST['paymentMode']);
+
+    //check customer phone
+    $check = "SELECT * FROM customers WHERE phone = '$cPhone' LIMIT 1";
+
+    $response = mysqli_query($conn, $check);
+    // print_r($response);
+    // exit();
+    if ($response) {
+
+        if (mysqli_num_rows($response) > 0) {
+            
+            $_SESSION['invoice_no'] = "INV - ".rand(000000, 999999);
+            $_SESSION['cPhone'] = $cPhone;
+            $_SESSION['payment_node'] = $paymentMode;
+            jsonResponse (200, 'success', 'customer found');
+
+
+        } else {
+            
+            $_SESSION['cPhone'] = $cPhone;
+            jsonResponse (400, 'warning', 'customer not found');
+        }
+
+    } else {
+        jsonResponse (500, 'error', 'something went wrong.');
+    }
 }
