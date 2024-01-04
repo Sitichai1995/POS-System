1a4efdadb490b3c85df295402fac8ac14f1dbc54
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2024-01-04 23:09:49 +0700

    Update custom.js

--- admin/assets/js/custom.js
+++ admin/assets/js/custom.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $(document).on('click','.increment', function () {
-        
+    $(document).on('click', '.increment', function () {
+
         let $quantityInput = $(this).closest('.qty-box').find('.qty');
         let productId = $(this).closest('.qty-box').find('.prodId').val();
 
         let currentValue = parseInt($quantityInput.val());
-        
+
         // console.log(currentValue);
         // return false
         if (!isNaN(currentValue)) {
-            let qtyVal = currentValue + 1 ;
+            let qtyVal = currentValue + 1;
             console.log(qtyVal);
-        
+
             $quantityInput.val(qtyVal);
-            quantityIncDec (productId, qtyVal)
+            quantityIncDec(productId, qtyVal)
         }
     });
 
-    
-    $(document).on('click','.decrement', function () {
-        
+
+    $(document).on('click', '.decrement', function () {
+
         let $quantityInput = $(this).closest('.qty-box').find('.qty');
         let productId = $(this).closest('.qty-box').find('.prodId').val();
 
@@ -29,15 +29,15 @@ $(document).ready(function(){
         // console.log(currentValue);
         // return false
         if (!isNaN(currentValue) && currentValue > 1) {
-            let qtyVal = currentValue - 1 ;
+            let qtyVal = currentValue - 1;
             console.log(qtyVal);
-        
+
             $quantityInput.val(qtyVal);
-            quantityIncDec (productId, qtyVal);
+            quantityIncDec(productId, qtyVal);
         }
     });
 
-    function quantityIncDec (prodId, qty){
+    function quantityIncDec(prodId, qty) {
         $.ajax({
             type: "POST",
             url: "order-code.php",
@@ -49,7 +49,7 @@ $(document).ready(function(){
             },
             success: function (response) {
                 let res = JSON.parse(response);
-                
+
                 if (res.status == 200) {
                     window.location.reload();
                     alertify().success(res.message);
@@ -62,33 +62,69 @@ $(document).ready(function(){
     }
 
     //proceed to place order button click
-    $(document).on('click','.proceedToPlace', function () {
+    $(document).on('click', '.proceedToPlace', function () {
 
         let cPhone = $('#cphone').val();
         let paymentMode = $('#payment_mode').val();
-
         if (paymentMode == '') {
-            swal("select payment Mode", "Please select your payment mode","warning");
+            swal("select payment Mode", "Please select your payment mode", "warning");
+            alert('test');
             return false;
         }
 
         if (cPhone == '' && !$.isNumeric(cPhone)) {
-            swal("Enter phone number", "Please Enter phone number","warning");
+            swal("Enter phone number", "Please Enter phone number", "warning");
             return false;
         }
-
+        
         let data = {
             'proceedToPlaceBtn': true,
             'cPhone': cPhone,
             'paymentMode': paymentMode
         };
 
+       
         $.ajax({
             type: "POST",
             url: "order-code.php",
             data: data,
             success: function (response) {
-                
+                // console.log(response);
+                // return false;
+                let res = JSON.parse(response);
+                // console.log(res);
+                // return false;
+                if (res.status == 200) {
+                    window.location.href = "order-summary.php";
+
+                } else if (res.status == 400) {
+
+                    swal(res.message, res.message, res.status_type, {
+                        buttons: {
+                            catch: {
+                                text: "Add cutomer",
+                                value: "catch"
+                            },
+                            cancel: "Cancel"
+                        }
+                    })
+                        .then((value) => {
+                            switch (value) {
+                                case "catch":
+                                $('#addCustomerModal').modal('show');
+                                //console.log('Pop the customer add modal.')
+                                break;
+                                default:
+                            }
+
+
+                        });
+
+                }else{
+                    swal(res.message, res.message, res.status_type);
+                }
+
+
             }
         });
     });
