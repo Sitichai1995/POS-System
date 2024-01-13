6d87ddb42df31b8a92621678a9a6ee37fac63a82
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2024-01-13 22:38:47 +0700

    Update orders-view-print.php

--- admin/orders-view-print.php
+++ admin/orders-view-print.php
@@ -11,72 +11,73 @@ include('includes/header.php');
 
         </div>
         <div class="card-body">
-            <?php
-            if (isset($_GET['track'])) {
-                $trackingNo = validate($_GET['track']);
-                if ($trackingNo == '') {
-            ?>
-                    <div class="text-center py-5">
-                        <h5>Please provide tracking number</h5>
-                        <div class="">
-                            <a href="orders.php" class="btn btn-secondary mt-4 w-25">Go back to orders.</a>
-                        </div>
-                    </div>
+            <div id="myBillingArea">
                 <?php
-                }
-
-                $orderQuery = "SELECT orders.*, customers.* FROM orders, customers WHERE customers.id = orders.id AND tracking_no='$trackingNo' LIMIT 1";
-                $orderQueryRes = mysqli_query($conn, $orderQuery);
-                if (!$orderQueryRes) {
-                    echo "<h5> Something went wrong.</h5>";
-                    return false;
-                }
-
-                if (mysqli_num_rows($orderQueryRes) > 0) {
-
-                    $rowData = mysqli_fetch_assoc($orderQueryRes);
-                    // print_r($rowData);
+                if (isset($_GET['track'])) {
+                    $trackingNo = validate($_GET['track']);
+                    if ($trackingNo == '') {
                 ?>
+                        <div class="text-center py-5">
+                            <h5>Please provide tracking number</h5>
+                            <div class="">
+                                <a href="orders.php" class="btn btn-secondary mt-4 w-25">Go back to orders.</a>
+                            </div>
+                        </div>
+                    <?php
+                    }
 
-                    <table style="width: 100%; margin-bottom: 20px;">
-                        <tr>
-                            <td style="text-align: center;">
-                                <h4 style="font-size: 23px; line-height: 30px; margin: 2px; padding: 0;">Customer Details</h4>
-                                <p style="font-size: 16px; line-height: 24px; margin: 2px; padding: 0;">23 street</p>
-                                <p style="font-size: 16px; line-height: 24px; margin: 2px; padding: 0;">company xyz</p>
-                            </td>
+                    $orderQuery = "SELECT orders.*, customers.* FROM orders, customers WHERE customers.id = orders.id AND tracking_no='$trackingNo' LIMIT 1";
+                    $orderQueryRes = mysqli_query($conn, $orderQuery);
+                    if (!$orderQueryRes) {
+                        echo "<h5> Something went wrong.</h5>";
+                        return false;
+                    }
 
-                            <td>
-                                <h5 style="font-size: 20px; line-height: 30px; margin: 0px; padding: 0;">Customer Details</h5>
-                                <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Name: <?= $rowData['name'] ?></p>
-                                <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Phone NO. <?= $rowData['phone'] ?></p>
-                                <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Email ID:<?= $rowData['email'] ?></p>
-                            </td>
+                    if (mysqli_num_rows($orderQueryRes) > 0) {
 
-                            <td align="end">
-                                <h5 style="font-size: 20px; line-height: 30px; margin: 0px; padding: 0;">Invoice Details</h5>
-                                <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Invoice No: <?= $rowData['tracking_no']; ?></p>
-                                <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Date <?= date('d M Y') ?></p>
-                                <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Address: 1st </p>
-                            </td>
-                        </tr>
-                    </table>
+                        $rowData = mysqli_fetch_assoc($orderQueryRes);
+                        // print_r($rowData);
+                    ?>
+
+                        <table style="width: 100%; margin-bottom: 20px;">
+                            <tr>
+                                <td style="text-align: center;">
+                                    <h4 style="font-size: 23px; line-height: 30px; margin: 2px; padding: 0;">Customer Details</h4>
+                                    <p style="font-size: 16px; line-height: 24px; margin: 2px; padding: 0;">23 street</p>
+                                    <p style="font-size: 16px; line-height: 24px; margin: 2px; padding: 0;">company xyz</p>
+                                </td>
+
+                                <td>
+                                    <h5 style="font-size: 20px; line-height: 30px; margin: 0px; padding: 0;">Customer Details</h5>
+                                    <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Name: <?= $rowData['name'] ?></p>
+                                    <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Phone NO. <?= $rowData['phone'] ?></p>
+                                    <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Email ID:<?= $rowData['email'] ?></p>
+                                </td>
+
+                                <td align="end">
+                                    <h5 style="font-size: 20px; line-height: 30px; margin: 0px; padding: 0;">Invoice Details</h5>
+                                    <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Invoice No: <?= $rowData['invoice_no']; ?></p>
+                                    <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Customer Date <?= date('d M Y') ?></p>
+                                    <p style="font-size: 14px; line-height: 20px; margin: 0px; padding: 0;">Address: 1st </p>
+                                </td>
+                            </tr>
+                        </table>
 
 
-                <?php
-                } else {
-                    echo "<h5> No Customer found.</h5>";
-                    return false;
-                }
+                        <?php
+                    } else {
+                        echo "<h5> No Customer found.</h5>";
+                        return false;
+                    }
 
-                $orderItemQuery = "SELECT order_items.quantity AS orderItemQuantity, order_items.price AS orderItemPrice, orders.*, order_items.*, products.* FROM orders, order_items, products WHERE order_items.order_id = orders.id AND products.id = order_items.product_id AND orders.tracking_no = '$trackingNo'";
+                    $orderItemQuery = "SELECT order_items.quantity AS orderItemQuantity, order_items.price AS orderItemPrice, orders.*, order_items.*, products.* FROM orders, order_items, products WHERE order_items.order_id = orders.id AND products.id = order_items.product_id AND orders.tracking_no = '$trackingNo'";
 
-                $orderItemsQueryRes = mysqli_query($conn, $orderItemQuery);
+                    $orderItemsQueryRes = mysqli_query($conn, $orderItemQuery);
 
-                if ($orderItemsQueryRes) {
-                    if (mysqli_num_rows($orderItemsQueryRes) > 0) {
+                    if ($orderItemsQueryRes) {
+                        if (mysqli_num_rows($orderItemsQueryRes) > 0) {
                         ?>
-                        <div class="table-responsive mb-3">
+                            <div class="table-responsive mb-3">
                                 <table style="width:100%" cellpadding="5">
                                     <thead>
                                         <tr>
@@ -90,9 +91,9 @@ include('includes/header.php');
                                     <tbody>
                                         <?php
                                         $i = 1;
-                                        
+
                                         foreach ($orderItemsQueryRes as $key => $row) :
-                                            
+
                                         ?>
                                             <tr>
                                                 <td style="border-bottom: 1px solid #ccc;"><?= $i++ ?></td>
@@ -117,26 +118,35 @@ include('includes/header.php');
 
                             </div>
 
-                        <?php
+                    <?php
 
-                    } else {
-                        echo "<h5> No Customer found.</h5>";
-                        return false;
+                        } else {
+                            echo "<h5> No Customer found.</h5>";
+                            return false;
+                        }
                     }
+                } else {
+                    ?>
+
+                    <div class="text-center py-5">
+                        <h5>No tracking Number Parameter Found</h5>
+                        <div class="">
+                            <a href="orders.php" class="btn btn-secondary mt-4 w-25">Go back to orders.</a>
+                        </div>
+                    </div>
+                <?php
                 }
-            } else {
+
                 ?>
 
-                <div class="text-center py-5">
-                    <h5>No tracking Number Parameter Found</h5>
-                    <div class="">
-                        <a href="orders.php" class="btn btn-secondary mt-4 w-25">Go back to orders.</a>
-                    </div>
-                </div>
-            <?php
-            }
 
-            ?>
+            </div>
+
+            <div class="mt-4 text-end">
+                <button class="btn btn-info px-4 mx-1" onclick="printMybillingArea()">Print</button>
+                <button class="btn btn-warning px-4 mx-1" onclick="downloadPDF('<?= $rowData['invoice_no']; ?>')">Download PDF</button>
+            </div>
+
 
         </div>
     </div>
