6e29577d634ffd770aecc422c97f704ade524086
Sitichai <98759009+Sitichai1995@users.noreply.github.com>
2024-01-15 15:40:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <?php include('includes/header.php')?>
   
-<div class="py-5">
+<div class="py-5" style="background-image: url('assets/images/pos-bg.jpg'); background-size: cover; height: 85vh ;">
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-12">
-                <h1>POS system</h1>
+            <div class="col-md-12" style="margin-top: 100px;">
 
+            <?php alertMessage();?>
+
+                <h1 class="mt-3" style="font-size: 100px;">POS system</h1>
+
+                <?php if (!isset($_SESSION['loggedIn'])): ?>
                 <a href="login.php" class="btn btn-primary mt-4">Login</a>
+
+                <?php endif; ?>
             </div>
         </div>
     </div>
